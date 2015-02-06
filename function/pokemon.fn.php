dea038fd7359512864c46cf8676ee2a2eb9b2955
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-02-06 16:01:35 +0100

    Update

--- function/pokemon.fn.php
+++ function/pokemon.fn.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * CRUD
+ *
+ * Create Read Update Delete
+ */
+
 function getAllPokemons($link) {
     $sql = 'SELECT * FROM pokemon';
     $query = mysqli_query($link, $sql);
@@ -7,16 +13,32 @@ function getAllPokemons($link) {
     return mysqli_fetch_all($query);
 }
 
-function createPokemon($link, array $pokemon) {
-    // name | hp | type | level | trainer | attack | defense | evolution_id
+function getPokemon($link, $id) {
+    $sql = "SELECT * FROM pokemon WHERE id = $id";
+    $query = mysqli_query($link, $sql);
 
+    return mysqli_fetch_assoc($query);
+}
+
+function createPokemon($link, array $pokemon) {
     $columns = ''; // 'name, type'
     $values  = ''; // '"Pikachu", "electric"'
 
     foreach ($pokemon as $column => $value) {
-
+        $columns .= $column.',';
+        $values  .= '"'.$value.'",';
     }
 
+    $columns = substr($columns, 0, -1);
+    $values  = substr($values, 0, -1);
+
     $sql = 'INSERT INTO pokemon ('.$columns.') VALUES ('.$values.')';
-    mysqli_query($link, $sql);
+
+    var_dump($sql);
+
+    return mysqli_query($link, $sql);
 }
+
+//TODO update
+
+//TODO delete
--- index.php
+++ index.php
@@ -9,15 +9,27 @@ $link = database_connect($parameters);
 
 $pokemons = getAllPokemons($link);
 
-var_dump($pokemons);
-
-
-
-
+//var_dump($pokemons);
 
 $pokemon = [
-    'name' => 'Pikachu',
-    'type' => 'electric',
+    'name'    => 'Mew',
+    'type'    => 'water',
+    'hp'      => 75,
+    'level'   => 99,
+    'attack'  => 45,
+    'defense' => 40,
 ];
 
-createPokemon($link, $pokemon);
+if (false === createPokemon($link, $pokemon)) {
+    var_dump(mysqli_error($link));
+}
+
+$pokemon = getPokemon($link, 999);
+
+if (null === $pokemon) {
+    echo 'Aucun pokémon avec cet id';
+}
+
+var_dump($pokemon);
+
+echo '<h1>'.$pokemon['name'].'</h1>';
